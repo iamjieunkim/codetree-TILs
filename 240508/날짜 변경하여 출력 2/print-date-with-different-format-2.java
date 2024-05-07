214e22ac0bfdbcf5da774820e173803d90d5015b
Jieun Kim <83564946+iamjieunkim@users.noreply.github.com>
2024-05-08 01:41:26 +0900

    Title: 날짜 변경하여 출력 2, Time: 144ms, Memory: 10MB, Status: Passed - CodeTree

--- 240508/날짜 변경하여 출력 2/print-date-with-different-format-2.java
+++ 240508/날짜 변경하여 출력 2/print-date-with-different-format-2.java
@@ -5,8 +5,8 @@ public class Main {
 
         Scanner sc = new Scanner(System.in);
         sc.useDelimiter("-");
-        int d = sc.nextInt();
         int m = sc.nextInt();
+        int d = sc.nextInt();
         int y = sc.nextInt();
         System.out.printf("%d.%d.%d", y, m, d);
 
