c051a5960fef8db7ef1e19212fab5f7b54e00565
Jieun Kim <83564946+iamjieunkim@users.noreply.github.com>
2024-05-09 01:55:29 +0900

    Title: 특정 조건 두 정수 비교, Time: 153ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240509/특정 조건 두 정수 비교/specific-comparison-of-two-natural-numbers.java
+++ 240509/특정 조건 두 정수 비교/specific-comparison-of-two-natural-numbers.java
@@ -14,9 +14,9 @@ public class Main {
         }
         
         if(a==b){
-            System.out.println("1");
+            System.out.print("1"+" ");
         }else{
-            System.out.println("0");
+            System.out.println("0"+" ");
         }
     }
 }
