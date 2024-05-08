8804172a023a8e09530a185edcb9dc5ee69998db
Jieun Kim <83564946+iamjieunkim@users.noreply.github.com>
2024-05-09 02:40:18 +0900

    Title: 두 사람, Time: 126ms, Memory: 10MB, Status: Passed - CodeTree

--- 240509/두 사람/two-person.java
+++ 240509/두 사람/two-person.java
@@ -4,26 +4,15 @@ public class Main {
     public static void main(String[] args){
 
         Scanner sc = new Scanner(System.in);
-        int age1 = sc.nextInt();
+        int n1 = sc.nextInt();
         String m1 = sc.next();
-        int age2 = sc.nextInt();
+        int n2 = sc.nextInt();
         String m2 = sc.next();
 
-        if(age1>=19){
-            if(m1.charAt(0)=='M'){
-                System.out.println(1);
-            }else{
-                System.out.println(0);
-            }
-        }else if(age2>=19){
-            if(m2.charAt(0)=='M'){
-                System.out.println(1);
-            }else{
-                System.out.println(0);
-            }
+        if((m1.charAt(0)=='M'&&n1>=19)||(m2.charAt(0)=='M'&&n2>=19)){
+            System.out.println(1);
+        }else{
+            System.out.println(0);
         }
-
-        
-
     }
 }
