01ce280f6c2c782b9beb4fa4cea54064c60a4c75
Jieun Kim <83564946+iamjieunkim@users.noreply.github.com>
2024-05-11 02:21:07 +0900

    Title: 가장 짧은 문자열, Time: 150ms, Memory: 9MB, Status: Wrong Answer - CodeTree

--- 240511/가장 짧은 문자열/shortest-string.java
+++ 240511/가장 짧은 문자열/shortest-string.java
@@ -1,23 +1,20 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
 
         Scanner sc = new Scanner(System.in);
-        String a1 = sc.next();
+        String s1 = sc.next();
         String s2 = sc.next();
         String s3 = sc.next();
 
-        int min = a1.length();
-        if(s2.length()<s3.length()){
-            if(min>s2.length()){
-                min=s2.length();
-            }
-        }else{
-            if(min>s3.length()){
-                min=s3.length();
-            }
-        }
-        System.out.println(min);
+        int arr[] = new int[3];
+        arr[0]=s1.length();
+        arr[1]=s2.length();
+        arr[2]=s3.length();
+
+        Arrays.sort(arr);
+        System.out.println(arr[0]);
     }
 }
