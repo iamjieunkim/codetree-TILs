52412eeade766c3b33297539578f8e361fdaf33e
Jieun Kim <83564946+iamjieunkim@users.noreply.github.com>
2024-05-28 22:51:16 +0900

    Title: 감소시키며 출력하기, Time: 87ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240528/감소시키며 출력하기/decrease-and-print.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String args[]){
+        
+        for(int i=17; i>4; i--){
+            System.out.print(i+" ");
+        }
+
+    }
+}
