e2468cda1f588853d01d29b5f36b01c343e9a899
Jieun Kim <83564946+iamjieunkim@users.noreply.github.com>
2024-07-15 16:49:13 +0900

    Title: a 부터 b 까지, Time: 121ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240715/a 부터 b 까지/a-to-b.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        int n = a;
+        
+
+        while(n<14){
+            if(n%2==1){
+                System.out.print(n+" ");
+                n*=2;
+            }else{
+                System.out.print(n+" ");
+                n+=3;
+            }
+        }
+    }
+}
