63d38bde8ee5aaec6b0b5e3a17e7bd98d86ca265
Jieun Kim <83564946+iamjieunkim@users.noreply.github.com>
2024-07-17 16:45:03 +0900

    Title: 배수의 개수 2, Time: 145ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240717/배수의 개수 2/number-of-multipliers-2.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int arr[] = new int[10];
+        int cnt = 0;
+
+        for(int i=0; i<10; i++){
+            arr[i] = sc.nextInt();
+            if(arr[i]%2==1){
+                cnt++;
+            }
+        }
+
+        System.out.println(cnt);
+    }
+}
