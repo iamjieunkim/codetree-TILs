bd4f6671d40cbc7d03cecb232497c21893763f55
Jieun Kim <83564946+iamjieunkim@users.noreply.github.com>
2024-07-23 18:12:58 +0900

    Title: 별표 출력하기 2, Time: 117ms, Memory: 10MB, Status: Passed - CodeTree

--- 240723/별표 출력하기 2/print-star-2.java
+++ 240723/별표 출력하기 2/print-star-2.java
@@ -4,8 +4,8 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 
-        for(int i=0; i<5; i++){
-            for(int j=5-i; j>0; j--){
+        for(int i=0; i<n; i++){
+            for(int j=n-i; j>0; j--){
                 System.out.print("*"+" ");
             }
             System.out.println();
