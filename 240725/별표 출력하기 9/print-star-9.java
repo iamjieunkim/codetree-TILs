a7da82dceeaf4c4c2cb36e446776f02ca791e60f
Jieun Kim <83564946+iamjieunkim@users.noreply.github.com>
2024-07-25 16:45:19 +0900

    Title: 별표 출력하기 9, Time: 137ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240725/별표 출력하기 9/print-star-9.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+
+
+        for(int i=0; i<n; i++){
+            for(int j=0; j<(n-1)-i; j++){
+                System.out.print(" ");
+                System.out.print(" ");
+            }
+            for(int q=0; q<2*i+1; q++){
+                System.out.print("*");
+                System.out.print(" ");
+            }
+            for(int j=0; j<(n-1)-i; j++){
+                System.out.print(" ");
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+
+    }
+}
